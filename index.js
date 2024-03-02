0f34cded1bcb8866439acdb5ee4cf85d9a31763a
fredy09rizky <161811371+fredy09rizky@users.noreply.github.com>
2024-03-02 21:55:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const Handlebars = require("handlebars");
+const fs = require("fs")
+const path = require("path")
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-// Konfigurasi Nodemailer
-const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'fredy09rizky@gmail.com',
-        pass: 'acpnmxmndtgomnqi'
-    }
+app.get("/api", (req, res) => {
+  res.json({ status: "Online" });
 });
+app.head("/on", (req, res) => {
+  res.json({ status: "Online" });
+});
+app.head("/", (req, res) => {
+  res.json({ status: "Online" });
+});
+app.get("/", (req, res) => {
+  res.json({ status: "Online" });
+});
+
 
 // Endpoint untuk mengirim email
-app.post('/send-email', (req, res) => {
-    const { to, subject, text } = req.body;
 
-    const mailOptions = {
-        from: 'fredy09rizky@gmail.com',
-        to: to,
-        subject: subject,
-        text: text
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-            res.status(500).json({ error: 'Email could not be sent' });
-        } else {
-            console.log('Email sent: ' + info.response);
-            res.json({ message: 'Email has been sent' });
-        }
-    });
-});
-
-app.listen(port, () => {
-    console.log("Server is running on port " +port);
-});
+app.post("/api/send-email", (req, res) => {
+  const { from, to, subject, text, user, password } = req.body;
+  // Konfigurasi Nodemailer
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: user,
+      pass: password,
+    },
+  });
+  // Replace Kode Verifikasi
+  const __dirname = path.resolve();
+  const filePath = path.join(__dirname, './email.html');
+  const source = fs.readFileSync(filePath, 'utf-8').toString();
+  const template = Handlebars.compile(source);
+  const replacements = {
+    txt: text
+  };
