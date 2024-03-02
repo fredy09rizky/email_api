335d200e16e2ad46f15bf990abfddc76b59aa5ce
fredy09rizky <161811371+fredy09rizky@users.noreply.github.com>
2024-03-02 21:59:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,6 @@ app.get("/api", (req, res) => {
 app.head("/on", (req, res) => {
   res.json({ status: "Online" });
 });
-app.head("/", (req, res) => {
-  res.json({ status: "Online" });
-});
 app.get("/", (req, res) => {
   res.json({ status: "Online" });
 });
@@ -43,3 +40,25 @@ app.post("/api/send-email", (req, res) => {
   const replacements = {
     txt: text
   };
+  const htmlToSend = template(replacements);
+  const mailOptions = {
+    from: from,
+    to: to,
+    subject: subject,
+    html:  htmlToSend,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ error: "Email could not be sent" });
+    } else {
+      console.log("Email sent: " + info.response);
+      res.status(200).json({ message: "Email has been sent" });
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
